fdade2b7262e0f74e49610ac842fa837f80b6b05
AlGohry <ahmad.3bdellatif@gmail.com>
2025-03-27 00:37:51 +0300

    fix: Update logo image path and remove unused about link in HeaderComponent

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -4,14 +4,13 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-between">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <img src="@/assets/logo.png" alt="Vue Logo" width="40" height="40" class="ms-2" />
+          <img src="@/assets/images/logo.png" alt="Vue Logo" width="40" height="40" class="ms-2" />
           <span class="fs-4">إدارة المستخدمين</span>
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><router-link to="/" class="nav-link px-2 text-white">الرئيسية</router-link></li>
           <li v-if="auth.user"><router-link to="/users" class="nav-link px-2 text-white">المستخدمين</router-link></li>
-          <li><router-link to="/about" class="nav-link px-2 text-white">عن الموقع</router-link></li>
           <li><router-link to="/profile" class="nav-link px-2 text-white">{{ userName }}</router-link></li>
         </ul>
 
